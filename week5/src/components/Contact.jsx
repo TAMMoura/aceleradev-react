9c94de80db9b966bf977d70071ef1ccbbebc7c07
Thiago Moura <thiago_moura@hotmail.com.br>
2020-05-15 23:57:48 -0300

    Add week5 challenge resolution

---
+++ week5/src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (   
+    <article className="contact" data-testid="contact">
+    <span className="contact__avatar">
+      <img src={this.props.data.avatar} alt={this.props.data.avatar}/>
+    </span>
+    <span className="contact__data">{this.props.data.name}</span>
+    <span className="contact__data">{this.props.data.phone}</span>
+    <span className="contact__data">{this.props.data.country}</span>
+    <span className="contact__data">{this.props.data.admissionDate}</span>
+    <span className="contact__data">{this.props.data.company}</span>
+    <span className="contact__data">{this.props.data.department}</span>
+  </article>
+);
+}
+}
+
+export default Contact;
